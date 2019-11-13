8e4a3c64be1c3df8f4e2659464d2a6ea4a94167d
ritesh <ritesh_sinha@ymail.com>
2019-11-13 13:09:03 -0500

    Adding a go listener that runs forever on the daemon.

--- main.go
+++ main.go
@@ -6,22 +6,32 @@ import (
 	"net"
 )
 
+var (
+	dataCh chan *ConnectionData
+	port   = "2000"
+)
+
 type ConnectionData struct {
 	data       []byte
 	remoteAddr string
 	localAddr  string
+	conn       net.Conn
 }
-
-var (
-	dataCh chan *ConnectionData
-)
 
 func listen(ch chan *ConnectionData) {
 
 	for {
-		data := <-ch
+		recvVal := <-ch
 		fmt.Println("Yay. Got a data")
-		fmt.Println(data)
+		fmt.Println(string(recvVal.data))
+
+		// Frame reply
+		replyMsg := "Hello " + string(recvVal.data)
+		// Write the message in the connection channel.
+		recvVal.conn.Write([]byte(replyMsg))
+
+		// Closing the connection
+		recvVal.conn.Close()
 	}
 
 }
@@ -29,17 +39,17 @@ func listen(ch chan *ConnectionData) {
 func main() {
 	dataCh = make(chan *ConnectionData)
 
-	//go listen(dataCh)
+	go listen(dataCh)
 
 	// Listen on TCP port 2000 on all available unicast and
 	// anycast IP addresses of the local system.
-	l, err := net.Listen("tcp", ":2000")
+	l, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer l.Close()
 
-	fmt.Println("Listening on port", 2000)
+	fmt.Println("P2p daemon listening on port", port)
 	for {
 		// Wait for a connection.
 		fmt.Println("Waiting for message")
@@ -52,21 +62,18 @@ func main() {
 		// Handle the connection in a new goroutine.
 		// The loop then returns to accepting, so that
 		// multiple connections may be served concurrently.
-		go func(c net.Conn) {
-
-			buf := make([]byte, 1024)
-			// Read the incoming connection into the buffer.
-			_, err := conn.Read(buf)
-			if err != nil {
-				fmt.Println("Error reading:", err.Error())
-			}
-			fmt.Println("Received message:", string(buf))
-
-			// Frame reply
-			replyMsg := "Hello " + string(buf)
-			// Write the message in the connection channel.
-			conn.Write([]byte(replyMsg))
-			c.Close()
-		}(conn)
+		buf := make([]byte, 1024)
+		// Read the incoming connection into the buffer.
+		_, err = conn.Read(buf)
+		if err != nil {
+			fmt.Printf("conn.Read failed!!")
+			continue
+		}
+		dataCh <- &ConnectionData{
+			remoteAddr: conn.RemoteAddr().String(),
+			localAddr:  conn.LocalAddr().String(),
+			data:       buf,
+			conn:       conn,
+		}
 	}
 }
