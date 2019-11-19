98eac3e74b5f82fd7b9a629fbf76f942c55f0d15
Satya <satya@satyas-MacBook-Pro.local>
2019-11-19 13:09:45 -0500

    Adding doubly circular linked list with access time of constant order

--- src/lib/types.go
+++ src/lib/types.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"fmt"
 	"net"
 )
 
@@ -16,6 +17,9 @@ const (
 	PeerManagerRequest //state change msg
 	PeerManagerResponse
 	Have
+	Active
+	Inactive
+	Seeder
 )
 
 type SeederPushMsg struct {
@@ -36,4 +40,91 @@ type ConnectionData struct {
 	// remoteAddr string
 	// localAddr  string
 	Conn net.Conn
+}
+
+// Doubly circular linkedlist
+type Node struct {
+	Val        string
+	Prev, Next *Node
+}
+
+type DCL struct {
+	Head, Tail   *Node
+	ValToNodeMap map[string]*Node
+}
+
+func NewDCL() *DCL {
+	return &DCL{Head: nil, Tail: nil, ValToNodeMap: make(map[string]*Node)}
+}
+
+func (dcl *DCL) Append(val string) bool {
+	_, ok := dcl.ValToNodeMap[val]
+	if ok {
+		GetInstance().Debug("value already exist %v\n", val)
+		return false
+	}
+	node := &Node{Val: val}
+	dcl.ValToNodeMap[val] = node
+	if dcl.Head == nil {
+		// first node
+		node.Next = node
+		node.Prev = node
+		dcl.Head = node
+		dcl.Tail = node
+	} else {
+		node.Next = dcl.Head
+		node.Prev = dcl.Tail
+		dcl.Head.Prev = node
+		dcl.Tail.Next = node
+		dcl.Tail = node
+	}
+	return true
+}
+
+func (dcl *DCL) Remove(val string) bool {
+	node, ok := dcl.ValToNodeMap[val]
+	if !ok {
+		GetInstance().Debug("Value does not exist: %v\n", val)
+		return false
+	}
+	delete(dcl.ValToNodeMap, val)
+	if node == dcl.Head && node == dcl.Tail {
+		dcl.Head = nil
+		dcl.Tail = nil
+	} else {
+		node.Prev.Next = node.Next
+		node.Next.Prev = node.Prev
+		if node == dcl.Head {
+			dcl.Head = node.Next
+		}
+		if node == dcl.Tail {
+			dcl.Tail = node.Prev
+		}
+	}
+	return true
+}
+
+func (dcl *DCL) Print() {
+	node := dcl.Head
+
+	for node != nil {
+		fmt.Printf("%v ", node.Val)
+		node = node.Next
+		if node == dcl.Head {
+			break
+		}
+	}
+	fmt.Println()
+}
+
+func (dcl *DCL) RPrint() {
+	node := dcl.Tail
+	for node != nil {
+		fmt.Printf("%v ", node.Val)
+		node = node.Prev
+		if node == dcl.Tail {
+			break
+		}
+	}
+	fmt.Println()
 }
